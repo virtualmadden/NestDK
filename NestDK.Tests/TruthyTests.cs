274033875f1972a21d729b85d28b3f3eef8b615a
Jonathan Madden <virtualmadden@users.noreply.github.com>
2018-03-27 12:30:48 -0700

    Solution boilerplate

---
+++ NestDK.Tests/TruthyTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace NestDK.Tests
+{
+    [TestFixture]
+    public class TruthyTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _theTruth = true;
+        }
+
+        private bool _theTruth;
+
+        [Test]
+        public void ShouldBeTrue()
+        {
+            _theTruth.Should().BeTrue();
+        }
+    }
+}
